98420020e7aee38227a63a7d5ce26e30f3f58a4d
adrianurbano30 <javoshine_adrian@hotmail.com>
2025-07-31 16:29:08 -0500

    se crearon carpetas para identificar paginas componentes servicios respectivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { SearchPageComponent } from './gifs/pages/search-page/search-page.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',loadComponent:()=> import('./gifs/pages/dashboard-page/dashboard-page.component').then(m=>m.DashboardPageComponent),
+    children:[
+      {
+        path: 'trending',loadComponent:()=> import('./gifs/pages/trending-page/trending-page.component').then(m=>m.TrendingPageComponent),
+      },
+     {
+        path: 'search',loadComponent:()=> import('./gifs/pages/search-page/search-page.component').then(m=>m.SearchPageComponent),
+     },
+    ]
+  },
+
+  {
+    path:'**',redirectTo: 'dashboard', pathMatch: 'full'
+  }
+];

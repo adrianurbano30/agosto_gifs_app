4871e4b8b8006e969f708ee5e3496132d01ac019
adrianurbano30 <javoshine_adrian@hotmail.com>
2025-07-31 18:39:51 -0500

    actualizacion en opciondes del dashboard agregando links a respectivas paginas creadas trending y buscar

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions{
+  icon:string;
+  label:string;
+  route:string;
+  sublabel:string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+
+
+  menuOptions:MenuOptions[]=[
+    {
+      icon:"casa",
+      label:'Trending',
+      sublabel:'Most popular gifs',
+      route:'/dashboard/trending'
+    },
+    {
+      icon:`buscador`,
+      label:'Buscador',
+      sublabel:'Buscar gifs',
+      route:'/dashboard/search'
+    }
+  ];
+
+}
